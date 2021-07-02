914ef6e3eb08acffb52c911a483cb79f717ba327
Rocio <arias.roocio@gmail.com>
2021-07-02 13:56:09 +0200

    LEAN : fix validationlogin / server route partners

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const { signupAdmin, loginAdmin} = require("../controllers/adminController")
-const { validationSignupAdmin, validationLoginAdmin} = require ("../middlewares/validationMiddlewares")
+const { validationSignupAdmin, validationLogin} = require ("../middlewares/validationMiddlewares")
 
 
 router.post("/signupAdmin",validationSignupAdmin, signupAdmin)
 
-router.post("/loginAdmin", validationLoginAdmin, loginAdmin)
+router.post("/loginAdmin", validationLogin, loginAdmin)
 
 module.exports = router
