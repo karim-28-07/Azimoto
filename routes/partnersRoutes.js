91c0cc935953862db8bee7c859f7fdf3e6fffca1
Rocio <arias.roocio@gmail.com>
2021-07-06 15:49:03 +0200

    modif partnersController et test routes ok

--- routes/partnersRoutes.js
+++ routes/partnersRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const { getPartners, findPartnersById, updatePartner, deletePartner } = require("../controllers/partnersController.js")
+const { getPartners, findPartnersById, updatePartner, deletePartner, addNewPartner } = require("../controllers/partnersController.js")
 
 
 router.get('/', getPartners)
-router.get("/partner/:id", findPartnersById)
-router.delete('/partner/:id',deletePartner)
+router.post('/', addNewPartner )
+router.get("/:id", findPartnersById)
+router.delete('/:id',deletePartner)
 router.patch('/:id/partnerupdate',updatePartner)
 
 
