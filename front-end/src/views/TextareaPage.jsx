cfd15b0c02e2ad3892dc42cb52d285260f6a8b9c
Amir <amirhatam136@gmail.com>
2021-07-09 09:41:57 +0200

    modification page F. R .T

--- front-end/src/views/TextareaPage.jsx
+++ front-end/src/views/TextareaPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const TextareaPage = () => {
+const TextareaPage = (props) => {
+    const [Textarea, setTextarea] = useState({});
+    
+  
+    console.log("TextareaPage",Textarea);
+
+    // console.log("props.text",props.text);
+
     return (
         <div className="form-group">
-          
             <textarea
             className="form-control"
-            id="exampleFormControlTextarea1"
+           id={props.id}
             rows="5"
+            onChange={(e) => setTextarea({response:e.target.value, id:props.id})}
             style={{width: "80%"}}
             />
         </div>
